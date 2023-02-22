efff5b05604deb92124dd413b620826c910ec00f
AtanasRaykovPapucharov <atanas.papucharov@gmail.com>
2023-02-22 11:20:45 +0200

    task-2 dima's solution

--- 4-Arrays/Homework/2-compare-lexicographically.js
+++ 4-Arrays/Homework/2-compare-lexicographically.js
@@ -10,14 +10,37 @@ let gets =
 
 //-----------------------------
 
-const a = gets();
-const b = gets();
-const value = a.localeCompare(b);
+// const a = gets();
+// const b = gets();
+// const value = a.localeCompare(b);
 
-if (value === 0) {
-  console.log("equal");
-} else if (value < 0) {
+// if (value === 0) {
+//   console.log("equal");
+// } else if (value < 0) {
+//   console.log("first");
+// } else {
+//   console.log("second");
+// }
+
+// dima
+let arr1 = gets().split("");
+let arr2 = gets().split("");
+
+let sum1 = 0;
+let sum2 = 0;
+
+for (let i = 0; i < arr1.length; i++) {
+  sum1 += arr1[i].charCodeAt(0);
+}
+
+for (let i = 0; i < arr2.length; i++) {
+  sum2 += arr2[i].charCodeAt(0);
+}
+
+if (sum1 < sum2) {
   console.log("first");
-} else {
+} else if (sum2 < sum1) {
   console.log("second");
+} else {
+  console.log("equal");
 }
